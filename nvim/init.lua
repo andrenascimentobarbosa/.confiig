75cfd11ddaa08297033de722dcd0613074cbe0c3
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2025-06-25 14:01:26 -0300

    nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,24 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- toggle absolute/relative number
+vim.keymap.set('n', '<C-n>', function()
+	vim.wo.relativenumber = not vim.wo.relativenumber
+end)
+
+-- number gray
+vim.cmd[[
+highlight LineNr guifg=#555555 ctermfg=242
+]]
+
+-- disable statusline globally
+vim.opt.laststatus = 0  
+
+-- tab width to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- normal cursor
+vim.opt.guicursor = "a:block"
